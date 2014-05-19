a789d4834f79ab7f1c5f6c15c93690fac81128de
T. Andrew Binkowski <abinkowski@uchicago.edu>
2014-05-19 12:32:48 -0500

    Initial Commit

---
+++ 2014-Spring-CustomViewControllerTransitions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  2014-Spring-CustomViewControllerTransitions
+//
+//  Created by T. Andrew Binkowski on 5/19/14.
+//  Copyright (c) 2014 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
